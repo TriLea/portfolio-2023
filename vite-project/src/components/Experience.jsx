d374f21bfb2c5af5fceb6414871f7492ade5a1bb
tristan <tlea5@uw.com>
2023-10-11 18:39:05 -0700

    tried to create a ferrofluid object

--- vite-project/src/components/Experience.jsx
+++ vite-project/src/components/Experience.jsx
@@ -1,9 +1,12 @@
 import { OrbitControls } from "@react-three/drei";
+import Ferro from "./Ferro";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <Ferro />
+      <ambientLight intensity={1} />
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />
