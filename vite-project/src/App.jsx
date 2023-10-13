096a0685a9ff6820a4dec1624ed839f871cffd17
tristan <tlea5@uw.com>
2023-10-12 17:59:26 -0700

    setup of routes, and router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,31 @@
+import React from 'react';
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './Home'; 
+// import Projects from './Projects';
+// import Contact from './Contact';
+// import Resume from './Resume';
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <Router>
+      <Routes> 
+        {/* <Route path="/home" element={<Home />} /> 
+        <Route path="/projects" element={<Projects />} /> 
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/resume" element={<Resume />} />  */}
+
+        <Route path="/" element={ 
+          <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+            <color attach="background" args={["#ececec"]} />
+            <Experience />
+          </Canvas>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- vite-project/src/components/Orb.jsx
+++ vite-project/src/components/Orb.jsx
@@ -1,56 +1,42 @@
 import React, { useState, useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { Html } from '@react-three/drei';
+import { useNavigate } from 'react-router-dom';
 
 export default function Orb(props) {
   const [expand, setExpand] = useState(false);
-  const [section, setSection] = useState("");
   const meshRef = useRef();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (expand && meshRef.current) {
-      // This is a simple scaling effect, you can replace this with any other transition effect
-      meshRef.current.scale.set(50, 50, 50); // Assuming this scale consumes the entire screen
+    if (expand) {
+      meshRef.current.scale.set(50, 50, 50);
       setTimeout(() => {
-        // Once the transition is complete, take action based on the section selected
-        switch (section) {
-          case 'home':
-            console.log("Transition to Home");
-            // Redirect to Home or show the Home section here
-            break;
-          case 'projects':
-            console.log("Transition to Projects");
-            // Redirect to Projects or show the Projects section here
-            break;
-          // ... handle other sections similarly
-        }
-      }, 1000); // This timeout should match your transition duration
+        navigate(`/${expand}`);
+      }, 1000);
     }
-  }, [expand]);
+  }, [expand, navigate]);
 
   return (
     <group {...props} dispose={null}>
       <mesh ref={meshRef} geometry={new THREE.SphereGeometry(1, 32, 32)} material={new THREE.MeshPhongMaterial({ color: 0x000000, shininess: 100 })} />
-
+  
       {!expand && (
         <>
-          <Html position={[1.2, 0, 0]}>
-            <button onClick={() => { setSection('home'); setExpand(true); }}>Home</button>
+          <Html position={[Math.cos(0), 0, Math.sin(0)]}>
+            <button onClick={() => setExpand('home')}>Home</button>
           </Html>
-
-          <Html position={[-1.2, 0, 0]}>
-            <button onClick={() => { setSection('projects'); setExpand(true); }}>Projects</button>
+          <Html position={[Math.cos(Math.PI / 2), 0, Math.sin(Math.PI / 2)]}>
+            <button onClick={() => setExpand('projects')}>Projects</button>
           </Html>
-
-          <Html position={[0, 1.2, 0]}>
-            <button onClick={() => { setSection('contact'); setExpand(true); }}>Contact</button>
+          <Html position={[Math.cos(Math.PI), 0, Math.sin(Math.PI)]}>
+            <button onClick={() => setExpand('contact')}>Contact</button>
           </Html>
-
-          <Html position={[0, -1.2, 0]}>
-            <button onClick={() => { setSection('resume'); setExpand(true); }}>Resume</button>
+          <Html position={[Math.cos(3 * Math.PI / 2), 0, Math.sin(3 * Math.PI / 2)]}>
+            <button onClick={() => setExpand('resume')}>Resume</button>
           </Html>
         </>
       )}
     </group>
-  );
+  );  
 }
