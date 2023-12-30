26a7f4bbedc971e824032031f24f26b5c2b80794
tristan <tlea5@uw.com>
2023-12-29 21:32:15 -0800

    changed paths new build

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tlea.dev/vite-project/',
+  base: '/vite-project/',
   plugins: [react()]
 })
